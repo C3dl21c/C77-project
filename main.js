3a271f2cc90c0735774ccbc02bf9a5baf6f20847
C3dl21c <119985707+C3dl21c@users.noreply.github.com>
2023-01-28 11:26:15 -0600

    Add files via upload

---
+++ main.js
@@ -0,0 +1,22 @@
+var first_name = "ranbir";
+var last_name = "kapoor";
+var full_name ="first_name + last_name";
+
+console.log(first_name);
+console.log(last_name);
+console.log(full_name);
+
+var a = 10;
+var b = 5;
+
+var add = a + b;
+console.log(add);
+
+var sub = a - b;
+console.log(sub);
+
+var multi = a * b;
+console.log(multi);
+
+var divide = a / b;
+console.log (divide)
